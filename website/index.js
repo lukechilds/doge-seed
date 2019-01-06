2dfef1c3e092d34e3bd49f8c9374ec23add1ee66
Luke Childs <lukechilds123@gmail.com>
2019-01-06 11:57:25 +0700

    Add basic logic

--- website/index.js
+++ website/index.js
@@ -1,1 +1,20 @@
-console.log('hello world');
+import 'babel-polyfill';
+import whenDomReady from 'when-dom-ready';
+import dogeSeed from '../';
+
+const main = async () => {
+  await whenDomReady();
+
+  const seedText = document.querySelector('.seed-text');
+  const bits = document.querySelector('.bits');
+  const generateSeedButton = document.querySelector('.generate-seed');
+
+  const generateSeed = () => seedText.innerText = dogeSeed(bits.value);
+
+  generateSeedButton.addEventListener('click', generateSeed);
+  bits.addEventListener('input', generateSeed);
+
+  generateSeed();
+};
+
+main();
