42fa37f92803f7df6eac151e835430f921a54d0b
Luke Childs <lukechilds123@gmail.com>
2019-01-10 09:15:28 +0700

    Colour seed phrase words

--- website/index.js
+++ website/index.js
@@ -9,7 +9,15 @@ const main = async () => {
   const bits = document.querySelector('.bits');
   const regenerateSeedButton = document.querySelector('.regenerate-seed');
 
-  const generateSeed = () => seedText.innerText = dogeSeed(bits.value);
+  const generateSeed = () => {
+    const seedPhrase = dogeSeed(bits.value);
+    const wrappedSeedPhrase = seedPhrase
+      .split(' ')
+      .map(word => `<span>${word}</span>`)
+      .join(' ');
+
+    seedText.innerHTML = wrappedSeedPhrase;
+  };
 
   regenerateSeedButton.addEventListener('click', generateSeed);
   bits.addEventListener('input', generateSeed);
