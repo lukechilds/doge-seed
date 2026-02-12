024fac4594b543561a6dfee8ad3ea8f7d3db919f
Luke Childs <lukechilds123@gmail.com>
2026-02-12 11:39:51 +0700

    Revert "swap  tex-to-svg to katex for massive bundle size savings"
    This reverts commit 9df6cac334a539c151b86f899adeb498be291fcd.

--- website/index.js
+++ website/index.js
@@ -5,15 +5,14 @@ import '@fontsource/bricolage-grotesque/600.css';
 import '@fontsource/bricolage-grotesque/700.css';
 import '@fontsource/bricolage-grotesque/800.css';
 import '@fontsource/dm-mono/400.css';
-import katex from 'katex';
-import 'katex/dist/katex.min.css';
+import teXToSVG from 'tex-to-svg';
 import {version} from '../package';
 import dogeSeed from '..';
 
 import reportWebVitals from './vercel-vitals';
 
 Array.from(document.querySelectorAll('[data-latex]')).forEach(element => {
-	katex.render(element.dataset.latex, element, {throwOnError: false});
+	element.innerHTML = teXToSVG(element.dataset.latex)
 });
 
 document.querySelector('.version').innerText = `v${version}`;
